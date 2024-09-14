196058102ed5da0106c07bd0f2b2e585478e8d35
Arun silawat <arunsilawat722362@gmail.com>
2024-09-14 16:21:42 +0530

    Stack

---
+++ stack.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+//push(),pop(),top(),empty(),
+int main(){
+   stack<int> st;
+   st.push(11);
+   st.push(20);
+   st.push(30);
+   st.push(40);
+   st.push(50);
+   st.push(60);
+   st.push(70);
+   st.push(80);
+   //cout<<"Top: "<<st.top()<<endl;
+   //st.pop(); st.pop();
+   //cout<<"Top: "<<st.top()<<endl;
+   //no indexing norange based,no itrerator based loop
+   //cout<<st.empty(); //return true false 0 or 1
+
+   while(!st.empty()){
+    cout<<st.top()<<" ";
+    st.pop();
+   }
+
+   //string reverse....
+   string s="contant";
+   stack<char> st1;
+   for(int i=0;i<s.size();i++){
+    st1.push(s[i]);
+   }
+
+    while(!st1.empty()){
+      cout<<st1.top();
+      st1.pop();
+   }
+}
