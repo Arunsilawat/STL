7160910e6d2a9a7b2577245e05c234faca88d421
Arun silawat <arunsilawat722362@gmail.com>
2024-09-24 16:16:56 +0530

    multimap

--- map 3type insert data.cpp
+++ map 3type insert data.cpp
@@ -7,12 +7,26 @@ int main(){
   m.insert({2,"banana"});
   m.insert(make_pair(3,"pineapple"));
   unordered_map<int,string>:: iterator it;
+
   for(it=m.begin();it!=m.end();it++){
     cout<<(*it).first<<" "<<(*it).second<<" ";
   }
   cout<<endl;
 
   for(auto i:m){
+    cout<<i.first<<" "<<i.second<<" ";
+  }
+  //------------------multimap-----------------------
+  cout<<endl;
+  multimap<int,string>m1;
+
+  m1.insert({2,"banana"});
+  m1.insert(make_pair(2,"pineapple"));
+
+
+  cout<<endl;
+
+  for(auto i:m1){
     cout<<i.first<<" "<<i.second<<" ";
   }
 }
